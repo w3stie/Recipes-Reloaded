59dbe6af366e0d69141e22cc96d678ada1e31dc7
w3stie <w3stie@users.noreply.github.com>
2024-07-16 17:32:47 +0300

    Header besign and build

---
+++ index.js
@@ -0,0 +1,28 @@
+/* Menu reveal */
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-button'),
+      navClose = document.getElementById('nav-close')
+
+//Show Menu
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+//Menu hidden
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+//Mobile Menu Remove
+const navLink = document.querySelectorAll('.nav-link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
