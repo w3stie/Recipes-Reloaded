305b1fcfb7c63e7477d5b51f253bfbda83b7b281
w3stie <w3stie@users.noreply.github.com>
2024-07-16 19:59:02 +0300

    Completed base hero section

--- index.js
+++ index.js
@@ -26,3 +26,12 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//Changing background header
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+
+        this.scrollY >= 50 ? header/classList.add('shadow-header')
+                           : header/classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
