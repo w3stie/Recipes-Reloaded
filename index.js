83a9b395e86e0a08d7ce9c07933f68662c425226
w3stie <w3stie@users.noreply.github.com>
2024-07-17 21:12:16 +0300

    Reworked the code for cleanr outcome

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
 /* Menu reveal */
-
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-button'),
-      navClose = document.getElementById('nav-close')
+      navClose = document.getElementById('nav-close');
 
-//Show Menu
-if(navToggle){
+// Show Menu
+if (navToggle) {
     navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+        navMenu.classList.add('show-menu');
+    });
 }
 
-//Menu hidden
-if(navClose){
+// Hide Menu
+if (navClose) {
     navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+        navMenu.classList.remove('show-menu');
+    });
 }
 
-//Mobile Menu Remove
-const navLink = document.querySelectorAll('.nav-link')
+// Mobile Menu Remove on Link Click
+const navLink = document.querySelectorAll('.nav-link');
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+    //on clicking each nav-link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-//Changing background header
+// Changing Background Header
 const shadowHeader = () => {
-    const header = document.getElementById('header')
-
-        this.scrollY >= 50 ? header.classList.add('shadow-header')
-                           : header.classList.remove('shadow-header')
+    const header = document.getElementById('header');
+    window.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
 }
-window.addEventListener('scroll', shadowHeader)
+window.addEventListener('scroll', shadowHeader);
+
+/* Show Scroll Up Button */
+const scrollUpButton = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    window.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUpButton);
+
+/* Scroll Sections Active Link */
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']');
+
+        if (sectionsClass) { // Check if sectionsClass is not null
+            if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+                sectionsClass.classList.add('active-link');
+            } else {
+                sectionsClass.classList.remove('active-link');
+            }
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
